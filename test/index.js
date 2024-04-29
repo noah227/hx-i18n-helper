1d4d821bab3cb1369765b776a34cfcdb69188b76
noah227 <noah227@foxmail.com>
2024-04-29 14:49:46 +0800

    chore: 更新测试示例及文档示例内容

--- test/index.js
+++ test/index.js
@@ -14,4 +14,8 @@ helper.syncLocales()
 // 生成一个简单的键值对辅助文件，对于js代码内需要使用i18n的地方进行智能推断辅助
 helper.generateJsHelper()
 // 自动判断locale并从i18n文件中获取内容，主要是在js代码中辅助使用
-// helper.i18nGet("")
+// helper.i18nGet("key", "这是默认值123")
+// 批量获取i18n内容，返回Object
+// helper.i18nGets(["key1", "key2"])
+// 也可以配置默认值
+// helper.i18nGets(["key1", ["key2", "这是默认值123"]])
